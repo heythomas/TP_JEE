c4529b5ab4d37827924f4e1a7eb6c10340a5a2c5
Thomas <thomas0bellon@gmail.com>
2021-10-08 11:26:41 +0200

    Objet colis

---
+++ src/main/java/model/Colis.java
@@ -0,0 +1,110 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+public class Colis implements Serializable{
+	// UID
+	private static final long serialVersionUID = 1L;
+
+	// Infos permanentes
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private double poids;
+	private double valeur;
+	private String origine;
+	private String destination;
+	
+	// Infos modifiables
+	private double latitude;
+	private double longitude;
+	private String emplacement;
+	private String etat;
+	
+	public void Colis(double poids,double valeur,String origine,String destination,double latitude,double longitude,String emplacement,String etat) {
+		this.poids = poids;
+		this.valeur = valeur;
+		this.origine = origine;
+		this.destination = destination;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.emplacement = emplacement;
+		this.etat = etat;		
+	}
+
+	public double getPoids() {
+		return poids;
+	}
+
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}
+
+	public double getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(double valeur) {
+		this.valeur = valeur;
+	}
+
+	public String getOrigine() {
+		return origine;
+	}
+
+	public void setOrigine(String origine) {
+		this.origine = origine;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getEmplacement() {
+		return emplacement;
+	}
+
+	public void setEmplacement(String emplacement) {
+		this.emplacement = emplacement;
+	}
+
+	public String getEtat() {
+		return etat;
+	}
+
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+}
